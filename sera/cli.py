70a63903ed1a2fb5dfc1199cfce4ba27a1a607f2
Brett Haydon <brett@haydon.id.au>
2017-01-31 14:52:22 +1100

    Handle plugin commands

--- sera/cli.py
+++ sera/cli.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
+import pkg_resources
 
 import click
+from pprint import pprint
 
 
 from .commands.main import main
@@ -16,3 +18,14 @@ from .commands.watch import watch
 from .commands.symlink import symlink
 from .commands.create import signature, keypair, access
 from .commands.crypt import encrypt, decrypt
+
+sources = [main]
+plugins = {}
+for ep in pkg_resources.iter_entry_points(group='sera.collection'):
+    sources.append(ep.load())
+
+for ep in pkg_resources.iter_entry_points(group='sera.subcommand'):
+    plugins[ep.name] = ep.load()
+
+cli = click.CommandCollection(sources=sources)
+
--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
                  'sera'},
     entry_points={
         'console_scripts': [
-            'sera=sera.cli:main'
+            'sera=sera.cli:cli'
         ]
     },
     include_package_data=True,
